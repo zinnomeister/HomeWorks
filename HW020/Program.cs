08edb7982283ab049c1465ae80cc1e578975850f
zinnomeister <zinnomeister@gmail.com>
2022-07-22 17:42:42 +0100

    Add project files.

---
+++ HW020/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace HW020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HW 020");
+
+            uint oddCount = 0; //нечётные
+            uint evenCount = 0; //чётные
+
+            int evenSum = 0;
+            int oddSum = 0;
+
+            Console.WriteLine("Input start value");
+            int currentValue = int.Parse(Console.ReadLine());
+            Console.WriteLine("Input ending value");
+            int limit = int.Parse(Console.ReadLine());
+
+            while (currentValue <= limit)
+            {
+                if (currentValue % 2 == 0)
+                {
+                    evenCount++;
+                    evenSum += currentValue;
+                }
+                else
+                {
+                    oddCount++;
+                    oddSum = oddSum + currentValue;
+                }
+                currentValue++;
+            }
+            Console.WriteLine("Quantity of odds: " + oddCount);
+            Console.WriteLine("Quantity of evens: " + evenCount);
+            Console.WriteLine("Sum of odds: " + oddSum);
+            Console.WriteLine("Sum of evens: " + evenSum);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SOHW-odd_even/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace HW020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HW by Sergei Oligarkhovich - odd/even");
+
+            //double oddCount = 0; //нечётные
+            // evenCount = 0; //чётные
+
+
+            Console.WriteLine("Input starting value");
+            double currentValue = double.Parse(Console.ReadLine());
+            Console.WriteLine("Input ending value");
+            double limit = double.Parse(Console.ReadLine());
+
+        Case:
+            Console.WriteLine("Input your choice");
+            Console.WriteLine("If you want to see odd numbers - press '1'");
+            Console.WriteLine("If you want to see even numbers - press '2'");
+
+            double choice = double.Parse(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+
+                    while (currentValue <= limit)
+                    {
+                        if (currentValue % 2 != 0)
+                        {
+                            Console.WriteLine(currentValue + ";");
+                            currentValue++;
+                            //evenSum += currentValue;
+                        }
+                        else
+                        {
+                            currentValue++;
+
+                        }
+                        //currentValue++;
+                    }
+                    break;
+
+                case 2:
+
+                    while (currentValue <= limit)
+                    {
+                        if (currentValue % 2 == 0)
+                        {
+                            Console.WriteLine(currentValue + ";");
+                            currentValue++;
+                            //evenSum += currentValue;
+                        }
+                        else
+                        {
+                            currentValue++;
+
+                        }
+                        //currentValue++;
+
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Wrong choice! Try again");
+                    goto Case;
+                    break;
+
+                //Console.WriteLine("Quantity of odds: " + oddCount);
+                //Console.WriteLine("Quantity of evens: " + evenCount);
+                //Console.WriteLine("Sum of odds: " + oddSum);
+                //Console.WriteLine("Sum of evens: " + evenSum);
+                //Exit:
+                    Console.ReadLine();
+            }
+        }
+    }
+}
