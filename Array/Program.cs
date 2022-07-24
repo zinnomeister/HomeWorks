fd2cdbf8a06d9d4c995910a914b2c197cd97b99e
zinnomeister <zinnomeister@gmail.com>
2022-07-24 19:04:40 +0100

    renew

---
+++ Array/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray;
+            myArray = new int[10];
+            myArray[0] = 1;
+            myArray[1] = 2;
+            myArray[2] = 3;
+            myArray[3] = 4;
+            myArray[4] = 5;
+            myArray[5] = 6;
+            myArray[6] = 7;
+            myArray[7] = 8;
+            myArray[8] = 9;
+            myArray[9] = 10;
+
+            Console.WriteLine(myArray[4]);
+            Console.WriteLine(myArray[6]);
+            Console.WriteLine(myArray[4]);
+
+
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
