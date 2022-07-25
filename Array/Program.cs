0493fd972807c8358f6e308de18eea09d75481b4
zinnomeister <zinnomeister@gmail.com>
2022-07-25 17:46:30 +0100

    220725

--- Array/Program.cs
+++ Array/Program.cs
@@ -19,12 +19,39 @@ namespace Array
             myArray[8] = 9;
             myArray[9] = 10;
 
-            Console.WriteLine(myArray[4]);
-            Console.WriteLine(myArray[6]);
-            Console.WriteLine(myArray[4]);
+            //Console.WriteLine(myArray[4]);
+            //Console.WriteLine(myArray[6]);
+            //Console.WriteLine(myArray[4]);
 
 
-            Console.ReadLine();
+            //Console.ReadLine();
+
+            //int[] myArray2 = new int[3];
+            //myArray2[0] = 10;
+            //myArray2[1] = 18;
+            //Console.ReadLine();
+
+            //int[] myArray3 = new int[3] { 1, 5, 8 };//input all!!!
+
+            //int[] myArray4 = new int[] { 2, 5, 6, 7, 9, 8 }; //[x]= quantity
+
+            //int[] myArray5 = new[] { 1, 4, 6, 8, 7, 5 };
+
+            //int[] myArray6 = { 1, 4, 5, 7, 8 };
+
+            //int[] myArray7 = Enumerable.Repeat(5, 3).ToArray();//3 of "5"
+            
+            int[] myArray8= Enumerable.Range(1, 15).ToArray();
+            for (int i = 0; i < myArray8.Length; i++)
+            {
+                Console.WriteLine(myArray8[i]);
+            }
+            
+
+            Console.ReadLine(); 
+
+
+
         }
     }
 }
