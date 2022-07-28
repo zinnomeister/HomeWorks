35a3d46451bcd5180b03528b60b7ffdd41251c6e
zinnomeister <zinnomeister@gmail.com>
2022-07-28 21:21:23 +0100

    Commit for SO

--- Indexes/Program.cs
+++ Indexes/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-//using System.Linq; ???
+using System.Linq;// ???
 
 namespace Array
 {
@@ -21,7 +21,41 @@ namespace Array
             }
 
 
+            Console.Write("\nFirst element:\t");
+            Console.WriteLine(Array[0]);
+           
+            Console.Write("\nLast element:\t");
             Console.WriteLine(Array[^1]);
+           
+            Console.Write("\nMinimal range:\t");
+            Console.WriteLine(Array.Min());
+           
+            Console.Write("\nMaximal range:\t");
+            Console.WriteLine(Array.Max());
+            
+            Console.Write("\nSumm of elements:\t");
+            Console.WriteLine(Array.Sum());
+           
+            Console.Write("\nSumm of even elements:\t");
+            Console.WriteLine(Array.Where(i => i % 2 == 0).Sum());
+           
+            Console.Write("\nSumm of odd elements:\t");
+            Console.WriteLine(Array.Where(i => i % 2 != 0).Sum());
+           
+            Console.WriteLine("\nSort elements by ascending order:");
+            int[] ArrayUp = Array.OrderBy(i => i).ToArray();
+            for (int i = 0; i < ArrayUp.Length; i++)
+                Console.WriteLine("\n" + ArrayUp[i]);
+            
+            Console.Write("\nSort elements by descending order:\t");
+            int[] ArrayDown = Array.OrderByDescending(i => i).ToArray();
+            for (int i = ArrayDown.Length - 1; i >= 0; i--)
+            
+                Console.WriteLine(ArrayDown[i]);
+            
+
+
+
 
             Console.ReadLine();
 
