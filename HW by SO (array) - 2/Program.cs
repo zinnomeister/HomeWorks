aef14e00c1e159741ddba0202b8e93fdbb8b7d1f
zinnomeister <zinnomeister@gmail.com>
2022-08-01 20:07:31 +0100

    220801

---
+++ HW by SO (array) - 2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using System.Linq;// ???
+
+namespace Array
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input sentence:\t");
+            string str = Console.ReadLine();
+
+            Console.WriteLine(str);
+            foreach (var item in str)
+            {
+                Console.WriteLine($"{item}");
+            }
+            //Console.ReadLine();
+
+            Console.Write("Input symbols:\t");
+            string str2 = Console.ReadLine();
+            Console.WriteLine(str2);
+
+
+        }
+    }
+}
