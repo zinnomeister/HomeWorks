82fe2f9c79d523e47bf2af4095f4d5a9c730ac94
zinnomeister <zinnomeister@gmail.com>
2022-08-05 00:18:22 +0100

    220804

---
+++ HW by SO (array)/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using System.Linq;// ???
+
+namespace CharArray
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input words:\n");
+            string Char = Console.ReadLine();
+            int symbolsCount = Char.Length;
+            char[] Array = new char[symbolsCount];
+            for (int i = 0; i < symbolsCount; i++)
+            {
+                Array[i] = Char[i];
+            }
+            Console.WriteLine("\nList of chars:\n");
+            foreach (char currentChar in Array)
+            {
+                Console.WriteLine(currentChar);
+            }
+           
+
+            Console.ReadKey();
+        }
+    }
+}
