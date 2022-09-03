767d597414038a2d7d6e56844b90efcc0bda0ee7
zinnomeister <zinnomeister@gmail.com>
2022-09-03 18:19:29 +0100

    add jaggedArr

---
+++ Jagged Array/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace JuggedArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] jaggedArray = new int[4][];
+            jaggedArray[0] = new int[5];
+            jaggedArray[1] = new int[3];
+            jaggedArray[2] = new int[8];
+            jaggedArray[3] = new int[6];
+
+            Random random = new Random();
+            for (int i = 0; i < jaggedArray.Length; i++)
+            {
+                for (int j = 0; j < jaggedArray[i].Length; j++)
+                {
+                    jaggedArray[i][j] = random.Next(1, 20);
+                }
+            }
+
+            for (int i = 0; i < jaggedArray.Length; i++)
+            {
+                for (int j = 0; j < jaggedArray[i].Length; j++)
+                {
+                    Console.Write(jaggedArray[i][j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
